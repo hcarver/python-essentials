cdd954343e3b572bc735b4f377a5aa1e75dc7ec0
Dave Millican <dave@skillerwhale.com>
2020-06-15 00:52:30 +0100

    Add missing exercises (#25)
    The original version of these exercises was missing the extension
    exercise on custom Iterators and the exercise on infinite iterables.
    This commit adds both of these.

--- iterators/iterators.py
+++ iterators/iterators.py
@@ -1,5 +1,7 @@
 """Python Iterators"""
 
+import time
+
 # The constants and function here are helpers for the exercises. You don't need
 # to change any of the code here (if you do, then the exercises might break!)
 
@@ -85,9 +87,18 @@ list of `Item`s.
 
 * Update the `ShoppingBasket` class so that it is an iterable. Iterating through
   the shopping basket should behave the same as iterating through its list of
-  items.
+  Items.
 
-* Extension -
+--------------------------------------------------------------------------------
+
+* Extension: If you finish the previous exercises then update the for loop,
+  so that instead of printing item.name, it prints item_name:
+
+  for item_name in my_basket:
+      print(item_name)
+
+* Now update ShoppingBasket so that it iterates through item names,
+  instead of the item objects. You will need to create a custom iterator class.
 """
 
 
@@ -181,3 +192,32 @@ class CustomRange:
 # print('\nRepeated squares up to 10 billion')
 # for i in CustomRange(2, 10000000000, lambda x: x ** 2):
 #     print(i)
+
+
+"""
+INFINITE ITERABLES
+------------------
+
+* Uncomment the lines of code at the bottom of this section. If you try to run
+  this program at the moment, it will break because InfiniteCycle is not an
+  iterable.
+
+* Without changing the code in the for loop, update the InfiniteCycle class so
+  that it is an iterable which cycles through all of its items without ever
+  terminating. For example:
+
+  for item in InfiniteCycle([1, 2, 3]):
+      print(item)
+
+  should print out the numbers 1, 2, 3, 1, 2, 3, 1, 2, 3, ... etc.
+"""
+
+class InfiniteCycle:
+    def __init__(self, items):
+        self.items = items
+
+
+# print("This will go on for")
+# for item in InfiniteCycle(["ever", "and"]):
+#     time.sleep(1)  # Wait 1 second
+#     print(item)
